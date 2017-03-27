f41f1b2c7f4fcf83d20d6aac730f22cb9e8477a0
Kingpin-27 <siddhu162@yahoo.com>
2017-03-28 00:14:13 +0530

    Create unrepeat.cpp

---
+++ unrepeat.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int unrepeat(int a[100],int n)
+{
+	int i,j,t;
+	for(i=0;i<n;i++)
+	{
+		t=0;
+		for(j=0;j<n;j++)
+		{
+			if(a[i]==a[j]&&i!=j)			
+				t=1;
+		}
+		if(t==0)
+		{
+			return a[i];
+			break;			
+		}
+	}
+}
+int main()
+{
+	int a[100],n,i;
+	cout<<"Enter the array limit : ";
+	cin>>n;
+	cout<<"Enter array elements\n";
+	for(i=0;i<n;i++)
+		cin>>a[i];
+	cout<<"The unrepeated number is "<<unrepeat(a,n);
+}
