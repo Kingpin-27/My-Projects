db89079f0611811083e820f891f6ad6b47907b09
Kingpin-27 <siddhu162@yahoo.com>
2017-05-13 16:46:48 +0530

    Create Balls.java

---
+++ Balls.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Balls 
+{
+	static int Blue,Red,Green,Yellow;
+	Balls()
+	{
+		Blue=Green=Red=Yellow=0;
+	}
+	static void print()
+	{
+		System.out.println("Red Balls    : "+Red);
+		System.out.println("Green Balls  : "+Green);
+		System.out.println("Blue Balls   : "+Blue);
+		System.out.println("Yellow Balls : "+Yellow);
+	}
+	static void colour(String a)
+	{ 
+		if(a=="b"||a=="B")
+			Blue=Blue+1;
+		if(a=="g"||a=="G")
+		{	
+			if(Green<3)
+				Green++;
+			else
+				System.out.println("Green Balls have exceeded limit(3)");
+		}	
+		if(a=="r"||a=="R")
+		{
+			if(Red==Green)
+				System.out.println("Red Balls can not be more than Green Balls");
+			else
+				Red++;
+		}
+		if(a=="y"||a=="Y")
+		{
+			if(Blue==0)
+				System.out.println("There must be a minimum of 1 Blue Ball before adding a Yellow Ball");
+			else
+				Yellow++;
+		}
+		print();
+	}
+	public static void main(String[] args) 
+	{					
+		System.out.print("Ball colour : ");
+		Scanner s = new Scanner(System.in);
+		String input = s.next();
+		colour(input);
+		System.out.print("Do you want to continue adding Balls? ");
+		String decide = s.next();
+		s.close();
+		if(decide=="Y"||decide=="y")
+			main(args);		
+	}
+
+}
